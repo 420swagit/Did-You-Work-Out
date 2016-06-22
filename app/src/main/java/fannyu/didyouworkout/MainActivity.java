dffbd0fc6e9a9a35506798a0b6b58a42b3164c90
Fannyu Chien <fannyu.chien@yahoo.com>
2016-06-22 15:50:29 +0800

    Modified user input

--- app/src/main/java/fannyu/didyouworkout/MainActivity.java
+++ app/src/main/java/fannyu/didyouworkout/MainActivity.java
@@ -2,6 +2,11 @@ package fannyu.didyouworkout;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
